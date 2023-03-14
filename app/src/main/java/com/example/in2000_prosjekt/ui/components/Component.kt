acccd01e82026ded60ac95fdd1867c3e13134ee0
Ebba Maja Olsson <ebbamaja1@gmail.com>
2023-03-14 15:10:22 +0100

    Bottom bar design

--- app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
+++ app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
@@ -1,11 +1,12 @@
 package com.example.in2000_prosjekt.ui.components
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.LocationOn
@@ -14,34 +15,78 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.in2000_prosjekt.R
 import com.example.in2000_prosjekt.ui.theme.Sikt_hvit
 import com.example.in2000_prosjekt.ui.theme.Sikt_lyseblå
 import com.example.in2000_prosjekt.ui.theme.Sikt_mellomblå
+import com.example.in2000_prosjekt.ui.theme.Sikt_mørkeblå
 
 @Composable
 fun Sikt_BottomBar() {
 
     BottomAppBar(
-        modifier = Modifier,
+        modifier = Modifier.clip(RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp)),
         containerColor = Sikt_hvit,
 
         ) {
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(Icons.Outlined.LocationOn, contentDescription = "Localized description", tint = Sikt_mellomblå)
-            //Text(text = "Utforsk")
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(Icons.Outlined.Favorite, contentDescription = "Localized description", tint = Sikt_mellomblå)
-            //Text(text = "Favoritter")
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(Icons.Outlined.Menu, "", tint = Sikt_mellomblå)
-            //Text(text = "Fjellvettreglene")
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+            Column (horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.width(120.dp)
+            ){
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Outlined.LocationOn,
+                        contentDescription = "Localized description",
+                        tint = Sikt_mellomblå,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Sikt_lyseblå)
+                            .padding(5.dp))
+                }
+                Text(text = "Utforsk")
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.width(120.dp)
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Outlined.Favorite,
+                        contentDescription = "Localized description",
+                        tint = Sikt_mellomblå,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Sikt_lyseblå)
+                            .padding(5.dp)
+                    )
+                }
+                Text(text = "Favoritter")
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.width(120.dp)
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Outlined.Menu,
+                        "",
+                        tint = Sikt_mellomblå,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Sikt_lyseblå)
+                            .padding(5.dp)
+                    )
+                }
+                Text(text = "Fjellvettreglene")
+            }
         }
     }
 }
+
 
 @Composable
 fun Sikt_topBar() {
--- app/src/main/java/com/example/in2000_prosjekt/ui/screens/MapboxScreen.kt
+++ app/src/main/java/com/example/in2000_prosjekt/ui/screens/MapboxScreen.kt
@@ -1,6 +1,9 @@
 package com.example.in2000_prosjekt.ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,14 +12,36 @@ import com.example.in2000_prosjekt.ui.components.Sikt_BottomBar
 import com.mapbox.maps.MapView
 import com.mapbox.maps.dsl.cameraOptions
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
     fun ShowMap(onNavigateToNext: () -> Unit){
 
-        Column(
+        /*Column(
             Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Sikt_BottomBar()
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                AndroidView(
+                    modifier = Modifier,
+                    factory = {context ->
+                        MapView(context).apply {
+                            getMapboxMap().loadStyleUri("mapbox://styles/elisabethb/clf6t1z9z00b101pen0rvc1fu/draft") {
+                                cameraOptions{
+                                    zoom(19.9)
+                                }
+                            }
+                        }
+                    }
+                )
+            }
+        }*/
+        Scaffold(bottomBar = { Sikt_BottomBar()}) {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
