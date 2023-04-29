2c535a4eadcbac581f2f95e71586c372fcb69ae0
synnemar <synne2006@hotmail.com>
2023-04-29 10:29:31 +0200

    Fiksa design på Sikt_LocationCard_Hour og lagt til på Sikt_LocationCard.

--- app/src/main/java/com/example/in2000_prosjekt/ui/components/Component_LocationCard.kt
+++ app/src/main/java/com/example/in2000_prosjekt/ui/components/Component_LocationCard.kt
@@ -1,6 +1,7 @@
 package com.example.in2000_prosjekt.ui.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,17 +12,23 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.in2000_prosjekt.R
+import com.example.in2000_prosjekt.ui.AlertInfo
+import com.example.in2000_prosjekt.ui.LocationInfo
+import com.example.in2000_prosjekt.ui.NowCastInfo
+import com.example.in2000_prosjekt.ui.SunriseInfo
 import com.example.in2000_prosjekt.ui.theme.*
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sikt_LocationCard(){
-
+    //weatherinfo: LocationInfo, nowcastinfo: NowCastInfo, sunriseinfo: SunriseInfo, alertinfo: MutableList<AlertInfo>
     Card(
         colors = CardDefaults.cardColors(Sikt_lyseblå),
         modifier = Modifier.padding(20.dp),
@@ -39,12 +46,12 @@ fun Sikt_LocationCard(){
             LazyRow(
                 horizontalArrangement = Arrangement.spacedBy(10.dp)
             ) {
-                item { Sikt_LocationCard_Hour() }
-                item { Sikt_LocationCard_Hour() }
-                item { Sikt_LocationCard_Hour() }
-                item { Sikt_LocationCard_Hour() }
-                item { Sikt_LocationCard_Hour() }
-                item { Sikt_LocationCard_Hour() }
+                item { Sikt_LocationCard_Hour(12,"skyet", 7) }
+                item { Sikt_LocationCard_Hour(13, "delvisskyet", 8) }
+                item { Sikt_LocationCard_Hour(14, "lettskyet", 8) }
+                item { Sikt_LocationCard_Hour(15, "klart", 7) }
+                item { Sikt_LocationCard_Hour(16, "skyet", 6) }
+                item { Sikt_LocationCard_Hour(17, "skyet", 4) }
             }
             Spacer(modifier = Modifier.size(20.dp))
             Text(text = "Langtidsvarsel: ", fontWeight = FontWeight.Bold)
@@ -67,22 +74,66 @@ fun Sikt_LocationCard(){
 }
 
 @Composable
-fun Sikt_LocationCard_Hour() {
+fun Sikt_LocationCard_Hour(time : Int, clouds : String, temp: Int) {
+
+    fun getCloudVisuals(clouds: String): Int {
+        return if (clouds == "skyet") {
+            R.drawable.small_clouds_both
+        } else if (clouds == "delvisskyet") {
+            R.drawable.small_clouds_big
+        } else if (clouds == "lettskyet") {
+            R.drawable.small_clouds_small
+        } else {
+            R.drawable.small_clouds_clear
+        }
+    }
+
+    fun getRightWeather(weather: String): String {
+        return if (weather == "skyet") {
+            "Meget dårlig sikt"
+        } else if (weather == "delvisskyet") {
+            "Dårlig sikt"
+        } else if (weather == "lettskyet") {
+            "Lettskyet"
+        } else {
+            "Klart vær"
+        }
+    }
+
+    fun getRightKm(km: String): String {
+        return if (km == "skyet") {
+            "> 1 km sikt"
+        } else if (km == "delvisskyet") {
+            "1-4 km sikt"
+        } else if (km == "lettskyet") {
+            "4-10 km sikt"
+        } else {
+            "< 10 km sikt"
+        }
+    }
 
     Card(
-        modifier = Modifier
-            .width(70.dp)
-            .height(80.dp),
-        shape = RoundedCornerShape(10.dp),
-        colors = CardDefaults.cardColors(Sikt_bakgrunnblå)
+        colors = CardDefaults.cardColors(Sikt_bakgrunnblå),
+        modifier = Modifier.width(110.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(2.dp)
         ) {
-            Text(text = "12:00", color = Sikt_sort, fontSize = 12.sp)
-            Spacer(modifier = Modifier.height(30.dp))
-            Sikt_skyillustasjon()
+            Text(text = "$time:00", color = Sikt_sort, fontSize = 12.sp, fontWeight = FontWeight.Bold)
+            Box(
+                modifier = Modifier.size(80.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = getCloudVisuals(clouds)),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Text(text = getRightKm(clouds), fontSize = 12.sp, color = Sikt_sort, fontWeight = FontWeight.Bold)
+            Text(text = getRightWeather(clouds), fontSize = 12.sp, color = Sikt_sort)
+            Text(text = "$temp°", color = Sikt_sort, fontSize = 20.sp, fontWeight = FontWeight.Bold)
         }
     }
 }
