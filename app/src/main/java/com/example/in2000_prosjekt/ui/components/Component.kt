193797aa65d67bf3602e456980be20619a336a5a
Thea Hermansen Bakke <theahb@ulrik.uio.no>
2023-05-16 13:33:36 +0200

    veiledning

--- app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
+++ app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
@@ -199,6 +199,7 @@ fun Sikt_BottomBar2( ) {
     }
 }
 
+//ADD: boolean for når den kommer fra favorite screen.
 @Composable
 fun Sikt_Header(location : String , height: Int, lat: Double, lon: Double, alertinfo: MutableList<AlertInfo>, viewModel: FavoriteViewModel) {
     Row(
@@ -288,25 +289,24 @@ fun Sikt_Header(location : String , height: Int, lat: Double, lon: Double, alert
                     tint = Sikt_mørkeblå
                 )
                 viewModel.addFavorite(Favorite(lon,lat,location,height))
-            }  else if(!checked && !alreadyFav) {
-                Icon(
-                    painterResource(id = R.drawable.outline_favorite),
-                    contentDescription = "Localized description",
-                    tint = Sikt_mørkeblå
-                )
-            } else if (!checked) {
-                Icon(
-                    painterResource(id = R.drawable.outline_favorite),
-                    contentDescription = "Localized description",
-                    tint = Sikt_mørkeblå
-                )
-                viewModel.deleteUpdate(lon, lat)
-            } else {
+            } else if(alreadyFav){
                 Icon(
                     Icons.Filled.Favorite,
                     contentDescription = "Localized description",
                     tint = Sikt_mørkeblå
                 )
+            }else{
+                Icon(
+                    painterResource(id = R.drawable.outline_favorite),
+                    contentDescription = "Localized description",
+                    tint = Sikt_mørkeblå
+                )
+                /*
+                if(alreadyFav){
+                    viewModel.deleteUpdate(lon, lat)
+                }
+
+                 */
             }
         }
     }
