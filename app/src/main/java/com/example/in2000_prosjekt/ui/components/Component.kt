d11d580227bd897d335a88d4a3253af929e1b05c
Thea Hermansen Bakke <theahb@ulrik.uio.no>
2023-05-16 12:56:41 +0200

    fisket errorhandling

--- app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
+++ app/src/main/java/com/example/in2000_prosjekt/ui/components/Component.kt
@@ -288,16 +288,20 @@ fun Sikt_Header(location : String , height: Int, lat: Double, lon: Double, alert
                     tint = Sikt_mørkeblå
                 )
                 viewModel.addFavorite(Favorite(lon,lat,location,height))
-            }  else if(!checked) {
+            }  else if(!checked && !alreadyFav) {
                 Icon(
                     painterResource(id = R.drawable.outline_favorite),
                     contentDescription = "Localized description",
                     tint = Sikt_mørkeblå
                 )
-                if(alreadyFav){
-                    viewModel.deleteUpdate(lon,lat)
-                }
-            } else if (alreadyFav){
+            } else if (!checked) {
+                Icon(
+                    painterResource(id = R.drawable.outline_favorite),
+                    contentDescription = "Localized description",
+                    tint = Sikt_mørkeblå
+                )
+                viewModel.deleteUpdate(lon, lat)
+            } else {
                 Icon(
                     Icons.Filled.Favorite,
                     contentDescription = "Localized description",
